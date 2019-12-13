defb87c26baae0a41ddd237b69023aae50ece1ae
Kyoichi <kyoichi@gmail.com>
2019-12-13 12:36:32 +0900

    Lesson 2. Attribute Binding

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var app = new Vue({
     el: '#app',
     data: {
-        product: 'Socks'
+        product: 'Socks',
+        image: './assets/vmSocks-green-onWhite.jpg',
+        link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks'
     }
 })
