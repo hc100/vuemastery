2bef5bce6207197e55d2e0d025d14da62314191b
Kyoichi <kyoichi@gmail.com>
2019-12-13 18:03:41 +0900

    Lession 4. List Rendering

--- main.js
+++ main.js
@@ -5,6 +5,18 @@ var app = new Vue({
         image: './assets/vmSocks-green-onWhite.jpg',
         link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
         inventory: 0,
-        onSale: false
+        onSale: false,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green"
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue"
+            }
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
     }
 })
