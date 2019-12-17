6a3673aaf999b1a05451db807a010e3402fc9941
Kyoichi <kyoichi@gmail.com>
2019-12-17 13:39:28 +0900

    Lesson 8. Components

--- main.js
+++ main.js
@@ -1,29 +1,91 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-        brand: 'Vue Mastery',
-        product: 'Socks',
-        selectedVariant: 0,
-        link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
-        inventory: 0,
-        onSale: false,
-        details: ["80% cotton", "20% polyester", "Gender-neutral"],
-        variants: [
-            {
-                variantId: 2234,
-                variantColor: "green",
-                variantImage: './assets/vmSocks-green.jpg',
-                variantQuantity: 10
-            },
-            {
-                variantId: 2235,
-                variantColor: "blue",
-                variantImage: './assets/vmSocks-blue.jpg',
-                variantQuantity: 0
-            }
-        ],
-        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
-        cart: 0
+Vue.component('product-details', {
+    props: {
+        details: {
+            type: Array,
+            required: true
+        }
+    },
+    template: `
+          <ul>
+            <li v-for="detail in details">{{ detail }}</li>
+          </ul>
+    `
+})
+
+Vue.component('product', {
+    props: {
+        premium: {
+            type: Boolean,
+            required: true
+        }
+    },
+    template: `      <div class="product">
+        <div class="product-image">
+          <img v-bind:src="image">
+        </div>
+
+        <div class="product-info">
+          <h1>{{ title }}</h1>
+          <a :href="link" target="_blank">More products like this</a>
+
+          <p v-if="inventory > 10">In Stock</p>
+          <p v-else-if="inventory<=10 && inventory > 0">Almost out of Stock</p>
+          <p v-else :class="{ outOfStock: !inStock }">Out of Stock</p>
+          <p>Shipping: {{ shipping}}</p>
+          <p>{{ sale }}</p>
+
+          <product-details :details="details"></product-details>
+
+          <ul>
+            <li v-for="size in sizes">{{ size }}</li>
+          </ul>
+
+          <div v-for="(variant, index) in variants"
+               :key="variant.variantId"
+               class="color-box"
+               :style="{ backgroundColor: variant.variantColor }"
+               @mouseover="updateProduct(index)">
+          </div>
+
+          <button @click="addToCart"
+                  :disabled="!inStock"
+                  :class="{ disabledButton: !inStock }">Add to Cart</button>
+          <button @click="removeFromCart">Remove from Cart</button>
+
+          <div class="cart">
+            <p>Cart({{cart}})</p>
+          </div>
+
+        </div>
+
+      </div>
+`,
+    data() {
+        return {
+            brand: 'Vue Mastery',
+            product: 'Socks',
+            selectedVariant: 0,
+            details: ["80% cotton", "20% polyester", "Gender-neutral"],
+            link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
+            inventory: 0,
+            onSale: false,
+            variants: [
+                {
+                    variantId: 2234,
+                    variantColor: "green",
+                    variantImage: './assets/vmSocks-green.jpg',
+                    variantQuantity: 10
+                },
+                {
+                    variantId: 2235,
+                    variantColor: "blue",
+                    variantImage: './assets/vmSocks-blue.jpg',
+                    variantQuantity: 0
+                }
+            ],
+            sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+            cart: 0
+        }
     },
     methods: {
         addToCart() {
@@ -53,7 +115,18 @@ var app = new Vue({
             } else {
                 return this.brand + ' ' + this.product + ' are not on sale'
             }
-
+        },
+        shipping() {
+            if (this.premium) {
+                return "free"
+            }
+            return "$2.99"
         }
+    }
+})
+var app = new Vue({
+    el: '#app',
+    data: {
+        premium: false
     }
 })
