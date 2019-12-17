6f710a4b394859b930b93609ca26db3ad7c0011e
Kyoichi <kyoichi@gmail.com>
2019-12-17 12:22:24 +0900

    Lesson 7. Computed Properties

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
 var app = new Vue({
     el: '#app',
     data: {
+        brand: 'Vue Mastery',
         product: 'Socks',
-        image: './assets/vmSocks-green.jpg',
+        selectedVariant: 0,
         link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
         inventory: 0,
         onSale: false,
-        inStock: false,
         details: ["80% cotton", "20% polyester", "Gender-neutral"],
         variants: [
             {
                 variantId: 2234,
                 variantColor: "green",
-                variantImage: './assets/vmSocks-green.jpg'
+                variantImage: './assets/vmSocks-green.jpg',
+                variantQuantity: 10
             },
             {
                 variantId: 2235,
                 variantColor: "blue",
-                variantImage: './assets/vmSocks-blue.jpg'
+                variantImage: './assets/vmSocks-blue.jpg',
+                variantQuantity: 0
             }
         ],
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
@@ -30,8 +32,28 @@ var app = new Vue({
         removeFromCart() {
             this.cart -= 1
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
+        updateProduct(index) {
+            this.selectedVariant = index
+            console.log(index)
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        sale() {
+            if (this.onSale) {
+                return this.brand + ' ' + this.product + ' are on sale!'
+            } else {
+                return this.brand + ' ' + this.product + ' are not on sale'
+            }
+
         }
     }
 })
