c4b524eb6e5414782f83f9ac500d58af7bf09664
Kyoichi <kyoichi@gmail.com>
2019-12-18 19:45:59 +0900

    Lesson 3. State & Getters

--- mastering-vuex/src/views/EventCreate.vue
+++ mastering-vuex/src/views/EventCreate.vue
@@ -1,3 +1,24 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>Create an Event, {{ user.name }}</h1>
+    <p>This event was created by {{ user.id }}</p>
+    <p>There are {{ catLength }} categories</p>
+    <p>{{ getEventById(2) }}</p>
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+    </ul>
+  </div>
 </template>
+
+<script>
+import { mapState, mapGetters } from 'vuex'
+
+export default {
+  computed: {
+    ...mapState(['user', 'categories']),
+    ...mapGetters(['catLength', 'getEventById'])
+  }
+}
+</script>
+
+<style scoped></style>
