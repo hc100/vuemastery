f345c745063a793f32714dbe2b5763d907912242
Kyoichi <kyoichi@gmail.com>
2019-12-19 20:54:19 +0900

    Lesson 5. Mutations & Actions Pt. 2

--- mastering-vuex/src/store.js
+++ mastering-vuex/src/store.js
@@ -14,13 +14,10 @@ export default new Vuex.Store({
       { id: 3, text: '...', done: true },
       { id: 4, text: '...', done: false }
     ],
-    events: [
-      { id: 1, text: '...', done: true },
-      { id: 2, text: '...', done: false },
-      { id: 3, text: '...', done: true },
-      { id: 4, text: '...', done: false }
-    ],
-    count: 0
+    events: [],
+    event: {},
+    count: 0,
+    resultnum: 0
   },
   mutations: {
     INCREMENT_COUNT(state, value) {
@@ -28,6 +25,15 @@ export default new Vuex.Store({
     },
     ADD_EVENT(state, event) {
       state.events.push(event)
+    },
+    SET_EVENTS(state, events) {
+      state.events = events
+    },
+    UPDATE_RESULTNUM(state, value) {
+      state.resultnum = value
+    },
+    SET_EVENT(state, event) {
+      state.event = event
     }
   },
   actions: {
@@ -35,9 +41,38 @@ export default new Vuex.Store({
       return EventService.postEvent(event).then(() => {
         commit('ADD_EVENT', event)
       })
+    },
+    fetchEvents({ commit }, { perPage, page }) {
+      EventService.getEvents(perPage, page)
+                  .then(response => {
+                    commit('UPDATE_RESULTNUM', parseInt(response.headers['x-total-count']))
+                    commit('SET_EVENTS', response.data)
+                  })
+                  .catch(error => {
+                    console.log('There was an error:', error.response)
+                  })
+    },
+    fetchEvent({ commit }, id) {
+      var event = this.getters.getEventById(id)
+      if (event) {
+        commit('SET_EVENT', event)
+      } else {
+        EventService.getEvent(id)
+                    .then(response => {
+                      commit('SET_EVENT', response.data)
+                    })
+                    .catch(error => {
+                      console.log('There was an error:', error.response)
+                    })
+      }
     }
   },
   getters: {
+    hasNextPage: state => page => {
+      if ((state.resultnum - (3*page)) > 0)
+        return true
+      return false
+    },
     catLength: state => {
       return state.categories.length
     },
