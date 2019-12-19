45cb5f32fb7b7c2f932e81f8d6b5e7ca87526029
Kyoichi <kyoichi@gmail.com>
2019-12-19 19:25:32 +0900

    Lesson 4. Mutations & Actions Pt. 1

--- mastering-vuex/src/store.js
+++ mastering-vuex/src/store.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import EventService from '@/services/EventService.js'
 
 Vue.use(Vuex)
 
@@ -18,10 +19,24 @@ export default new Vuex.Store({
       { id: 2, text: '...', done: false },
       { id: 3, text: '...', done: true },
       { id: 4, text: '...', done: false }
-    ]
+    ],
+    count: 0
   },
-  mutations: {},
-  actions: {},
+  mutations: {
+    INCREMENT_COUNT(state, value) {
+      state.count += value
+    },
+    ADD_EVENT(state, event) {
+      state.events.push(event)
+    }
+  },
+  actions: {
+    createEvent({ commit }, event) {
+      return EventService.postEvent(event).then(() => {
+        commit('ADD_EVENT', event)
+      })
+    }
+  },
   getters: {
     catLength: state => {
       return state.categories.length
