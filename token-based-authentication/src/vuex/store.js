b3655be590ed7e09e6b45f420dae9978475fc287
Kyoichi Ozaki <k@bancho.jp>
2019-12-24 22:19:25 +0900

    Lesson 5. User Logout

--- token-based-authentication/src/vuex/store.js
+++ token-based-authentication/src/vuex/store.js
@@ -13,6 +13,10 @@ export default new Vuex.Store({
       state.user = userData
       localStorage.setItem('user', JSON.stringify(userData))
       axios.defaults.headers.common['Authorization'] = `Bearer ${userData.token}`
+    },
+    CLEAR_USER_DATA (state) {
+      localStorage.removeItem('user')
+      location.reload()
     }
   },
   actions: {
@@ -30,6 +34,9 @@ export default new Vuex.Store({
         .then(({ data }) => {
           commit('SET_USER_DATA', data)
         })
+    },
+    logout ({ commit }) {
+      commit('CLEAR_USER_DATA')
     }
   },
   getters: {
