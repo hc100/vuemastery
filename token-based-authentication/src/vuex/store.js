99517d4dd503242b511644f58a6fc2ea488b18a8
Kyoichi Ozaki <k@bancho.jp>
2019-12-24 22:03:36 +0900

    Lesson 4. User Login

--- token-based-authentication/src/vuex/store.js
+++ token-based-authentication/src/vuex/store.js
@@ -12,7 +12,7 @@ export default new Vuex.Store({
     SET_USER_DATA (state, userData) {
       state.user = userData
       localStorage.setItem('user', JSON.stringify(userData))
-      axios.defaults.headers.common['authorization'] = `Bearer ${userData.token}`
+      axios.defaults.headers.common['Authorization'] = `Bearer ${userData.token}`
     }
   },
   actions: {
@@ -23,6 +23,18 @@ export default new Vuex.Store({
           console.log('user data is', data)
           commit('SET_USER_DATA', data)
         })
+    },
+    login ({ commit }, credentials) {
+      return axios
+        .post('//localhost:3000/login', credentials)
+        .then(({ data }) => {
+          commit('SET_USER_DATA', data)
+        })
+    }
+  },
+  getters: {
+    loggedIn (state) {
+      return !!state.user
     }
   }
 })
